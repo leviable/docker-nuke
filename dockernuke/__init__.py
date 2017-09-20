c0ece49a3db54a1f7bfb969b6f9eda22391c7d1d
levi-rs <levi.noecker@rewardstyle.com>
2017-09-20 08:00:20 -0500

    Add basic nuking

--- dockernuke/__init__.py
+++ dockernuke/__init__.py
@@ -1,1 +1,6 @@
-__version__ = '0.0.1'
+from pbr.version import VersionInfo
+
+from .main import main  as nuke  # NOQA
+
+__version__ = VersionInfo('traw').semantic_version().release_string()
+__all__ = ('__version__', 'nuke')
--- dockernuke/main.py
+++ dockernuke/main.py
@@ -1,2 +1,21 @@
+import docker
+
+
 def main():
-    pass
+    client = docker.from_env()
+
+    for running_container in client.containers.list():
+        print("Stopping container: {0}".format(running_container.name))
+        running_container.stop()
+
+    for container in client.containers.list(all=True):
+        print("Removing container: {0}".format(container.name))
+        container.remove()
+
+    for image in client.images.list():
+        print("Removing image: {0}".format(image.tags))
+        client.images.remove(image.id)
+
+    for volume in client.volumes.list():
+        print("Removing volume: {0}".format(volume.name))
+        volume.remove()
