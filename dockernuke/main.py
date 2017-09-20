96d4febb5904ddb8b73aa8766fb2e2964d52eda6
levi-rs <levi.noecker@rewardstyle.com>
2017-09-20 12:49:37 -0500

    Fix version setter and log msg formatting

--- dockernuke/main.py
+++ dockernuke/main.py
@@ -10,17 +10,17 @@ def main():
     client = docker.from_env()
 
     for running_container in client.containers.list():
-        logging.info("Stopping container: {0}".format(running_container.name))
+        logging.info("Stopping container: %s", running_container.name)
         running_container.stop()
 
     for container in client.containers.list(all=True):
-        logging.info("Removing container: {0}".format(container.name))
+        logging.info("Removing container: %s", container.name)
         container.remove()
 
     for image in client.images.list():
-        logging.info("Removing image: {0}".format(image.tags))
+        logging.info("Removing image: %s", image.tags)
         client.images.remove(image.id)
 
     for volume in client.volumes.list():
-        logging.info("Removing volume: {0}".format(volume.name))
+        logging.info("Removing volume: %s", volume.name)
         volume.remove()
